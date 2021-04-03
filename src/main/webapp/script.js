f4f6fbca14e3ad2e0d5cb620ae46c021c4d29fc3
SylvesterElormCoch <sylvester.e.coch.22@dartmouth.edu>
2021-04-03 02:44:49 +0000

    set up navbar toggle and form display

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -15,9 +15,73 @@
 let map;
 
 // Attach your callback function to the `window` object
-window.initMap = function() {
-  map = new google.maps.Map(document.getElementById("maps"), {
-      center: { lat: 40.7484405, lng: -73.9878584 },
-      zoom: 4
-  });
+window.initMap = function () {
+    map = new google.maps.Map(document.getElementById("maps"), {
+        center: { lat: 40.7484405, lng: -73.9878584 },
+        zoom: 4
+    });
 };
+
+let interests = ['running', 'hiking', 'eating', 'walking', 'hiking'];
+
+function openMenu(event, tabName) {
+    // find relevant nodes
+    const tabcontents = document.querySelectorAll(".tabcontent");
+    const tablinks = document.querySelectorAll(".tabs a");
+
+    // remove everything with tabcontent class */
+    tabcontents.forEach((tabmenu) => (tabmenu.style.display = "none"));
+
+    // remove all tablinks active classes */
+    tablinks.forEach((tablink) => tablink.classList.remove("active"));
+
+    // add active class on current 
+    event.currentTarget.classList.add("active");
+    let currElement = document.getElementById(tabName)
+    currElement.style.display = "block";
+
+    if (tabName == "interests") {
+        createUserInterestsForm(interests);
+    }
+}
+
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    const formJSON = Object.fromEntries(data.entries());
+
+    // for multi-selects, we need special handling
+    formJSON.workout_interests = data.getAll('workout_interests');
+
+    results = JSON.stringify(formJSON, null, 2);
+    console.log("results", results);
+
+       
+        
+    // send over to api
+
+}
+
+
+
+
+function createUserInterestsForm(defaultInterests) {
+    var html = defaultInterests.map(function (interest) {
+        return (
+            `<div><input id="activity-${interest}" name="workout_interests" type="checkbox" value="${interest}">
+                 <label class="inline" for="${interest}">${interest}</label></div>`
+        )
+    }).join('');
+
+
+    html += `<div> + Add something else</div>
+             <div class="inline">Skip</div>
+             <div class="inline">Next</div>
+             <button type="submit">Done</button>`
+
+    const form = document.querySelector(".user_input");
+    form.innerHTML = html;
+}
+
